260051707a7323bf6d89ae157dda4f45997c2a16
happilyafter <happilyafter.invitation@gmail.com>
2021-01-04 00:08:14 +0700

    commit pertama

---
+++ js/main.js
@@ -0,0 +1,51 @@
+!(function($) {
+    "use strict";
+    
+    // Activate smooth scroll on page load with hash links in the url
+    $(document).ready(function() {
+        if (window.location.hash) {
+        var initial_nav = window.location.hash;
+        if ($(initial_nav).length) {
+            var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+            $('html, body').animate({
+            scrollTop: scrollto
+            }, 1500, 'easeInOutExpo');
+        }
+        }
+    });
+
+    // Hero-Carousel
+    $('.carousel').carousel({
+        interval: 5000,
+        Wrap: false,
+        ride: true,
+        touch: true,
+        pause: false
+    })
+  
+    // Set the count down timer
+    if ($('.countdown').length) {
+        var count = $('.countdown').data('count');
+        var template = $('.countdown').html();
+        $('.countdown').countdown(count, function(event) {
+            $(this).html(event.strftime(template));
+        });
+    }
+
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.back-to-top').fadeIn('slow');
+        } else {
+        $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function() {
+        $('html, body').animate({
+        scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+})(jQuery);
